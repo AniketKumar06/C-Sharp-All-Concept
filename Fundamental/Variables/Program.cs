adfe60806a0b0fb252ee6d34eb92e3e46d91236f
Aniket Kumar <aniketcse4158@.com>
2023-03-15 13:58:47 +0530

    Fundamental C SharpLangauge

--- Fundamental/Variables/Program.cs
+++ Fundamental/Variables/Program.cs
@@ -4,8 +4,10 @@ namespace Variables
 {
     public class Program
     {
+        int c;
         public static void Main(string[] args)
         {
+            
             /*
             Two Ways for Initialization:
             
@@ -13,6 +15,15 @@ namespace Variables
             02. Run time initialization
             
             */
+
+            // compile time initialization
+
+            int d = 12;
+            int e;
+            Program p = new Program();
+            Console.WriteLine($"The Value of d is {d} ");
+            Console.WriteLine($"The value of c is {p.c}");
+            // Console.WriteLine($"The value of e is {e}");    Error
         }
     }
 }
