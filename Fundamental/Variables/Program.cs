9154b1671ec29141cd28ba1874cb5158e59c27b0
Aniket Kumar <aniketcse4158@.com>
2023-03-15 13:44:55 +0530

    Fundamental C SharpLangauge

---
+++ Fundamental/Variables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Variables
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*
+            Two Ways for Initialization:
+            
+            01. Compile time initialization
+            02. Run time initialization
+            
+            */
+        }
+    }
+}
