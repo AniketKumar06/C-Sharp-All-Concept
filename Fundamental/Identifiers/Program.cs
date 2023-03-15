4426c826c02e57c49b963abdc90a727737c54de8
Aniket Kumar <aniketcse4158@.com>
2023-03-15 09:47:21 +0530

    Fundamentals C Sharp

---
+++ Fundamental/Identifiers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace  Identifiers
+{
+    public class Program{
+        public static void Main(string[] args)
+        {
+            int a = 10 ; 
+            int b = 39;
+            int c ;
+
+            c= a+b;
+            Console.WriteLine("The sum of Two number  is {0}",c);
+        }
+    }
+}
