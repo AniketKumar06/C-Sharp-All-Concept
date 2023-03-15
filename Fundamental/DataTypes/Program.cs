0c81bf9a54e33c8a00cbb7632fc259444538294b
Aniket Kumar <aniketcse4158@.com>
2023-03-15 11:32:45 +0530

    Fundamental Concept of C Sharp

--- Fundamental/DataTypes/Program.cs
+++ Fundamental/DataTypes/Program.cs
@@ -1,2 +1,87 @@
-﻿
+﻿using System;
+
+namespace DataTypes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*
+                    Data Types in C# is mainly divided in three categories
+                
+                    1. Value Data Types
+                    2. Reference Data Types
+                    3. Pointer Data Types
+
+             */
+
+            // Value Data Types
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine(
+                "sbyte DataType having size is {0} , Minvalue is {1} and Maxvalue is  {2}",
+                sizeof(sbyte),
+                sbyte.MinValue,
+                sbyte.MaxValue
+            );
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine(
+                "short DataType having size is {0} , Minvalue is {1} and Maxvalue is  {2}",
+                sizeof(short),
+                short.MinValue,
+                short.MaxValue
+            );
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine(
+                "int DataType having size is {0} , Minvalue is {1} and Maxvalue is  {2}",
+                sizeof(int),
+                int.MinValue,
+                int.MaxValue
+            );
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine(
+                "long DataType having size is {0} , Minvalue is {1} and Maxvalue is  {2}",
+                sizeof(long),
+                long.MinValue,
+                long.MaxValue
+            );
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine(
+                "byte DataType having size is {0} , Minvalue is {1} and Maxvalue is  {2}",
+                sizeof(byte),
+                byte.MinValue,
+                byte.MaxValue
+            );
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine(
+                "ushort DataType having size is {0} , Minvalue is {1} and Maxvalue is  {2}",
+                sizeof(ushort),
+                ushort.MinValue,
+                ushort.MaxValue
+            );
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine(
+                "uint DataType having size is {0} , Minvalue is {1} and Maxvalue is  {2}",
+                sizeof(uint),
+                uint.MinValue,
+                uint.MaxValue
+            );
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine(
+                "ulong DataType having size is {0} , Minvalue is {1} and Maxvalue is  {2}",
+                sizeof(ulong),
+                ulong.MinValue,
+                ulong.MaxValue
+            );
+            Console.WriteLine("*****************************************************************");
+        }
+    }
+}
+
 
