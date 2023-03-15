86dc7b919f6c3db751621edcbdbb852ec34825e5
Aniket Kumar <aniketcse4158@.com>
2023-03-15 16:43:09 +0530

    Fundamental C SharpLangauge

--- Fundamental/TypesOfVariables/Program.cs
+++ Fundamental/TypesOfVariables/Program.cs
@@ -18,7 +18,18 @@ namespace TypesOfVariables
             
              */
 
-             
+
+            //  Local Variable
+            name();
+            Console.WriteLine("Value of c is " + c);   // Error beacuse c is define as local variable
+
+        }
+
+        public static void name(){
+            int c = 23;
+            Console.WriteLine("Value of c is " + c);
         }
     }
+
+  
 }
