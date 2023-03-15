b1c31f96b000e3d2e7abf0f5a24bd0232c31ab8b
Aniket Kumar <aniketcse4158@.com>
2023-03-15 15:36:56 +0530

    Fundamental C SharpLangauge

---
+++ Fundamental/TypesOfVariables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TypesOfVariables
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*
+            
+            Types of Variables
+                01. Local variables
+                02. Instance variables or Non – Static Variables
+                03. Static Variables or Class Variables
+                04. Constant Variables
+                05. Readonly Variables
+ 
+            
+             */
+
+             
+        }
+    }
+}
