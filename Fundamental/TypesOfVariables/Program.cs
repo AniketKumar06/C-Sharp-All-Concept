8088e6609fae42b2b41fd4501d02b3f97c90ffce
Aniket Kumar <aniketcse4158@.com>
2023-03-22 09:11:29 +0530

    cSharp

--- Fundamental/TypesOfVariables/Program.cs
+++ Fundamental/TypesOfVariables/Program.cs
@@ -6,25 +6,13 @@ namespace TypesOfVariables
     {
         public static void Main(string[] args)
         {
-            /*
-            
-            Types of Variables
-                01. Local variables
-                02. Instance variables or Non – Static Variables
-                03. Static Variables or Class Variables
-                04. Constant Variables
-                05. Readonly Variables
- 
-            
-             */
-
 
             //  Local Variable
             name();
             Console.WriteLine("Value of c is " + c);   // Error beacuse c is define as local variable
 
         }
-
+            
         public static void name(){
             int c = 23;
             Console.WriteLine("Value of c is " + c);
